f3d23e41f8517b172d9248b5861f7ff31d331842
TheRogueNPC <Team.Crow@yahoo.com>
2026-07-01 17:53:59 -0700

    chore(vite): set Vite root to frontend and adjust build dir for Tauri

--- vite.config.tauri.ts
+++ vite.config.tauri.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import tauri from './src-tauri/plugins/vite-plugin-tauri';
 
 export default defineConfig({
+  root: 'src/frontend',
   plugins: [tauri()],
   clearScreen: false,
   server: {
     open: false,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
